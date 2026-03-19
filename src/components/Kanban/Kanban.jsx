4666c8befe449aa590881f5d2fef60fb7e7947a6
Adan Adeel <adanadeel903@gmail.com>
2026-03-19 15:49:29 +0500

    Kanban Done

--- src/components/Kanban/Kanban.jsx
+++ src/components/Kanban/Kanban.jsx
@@ -41,7 +41,7 @@ function Kanban() {
         if (newTask.trim() === "") return;
         const updateColumns = {...columns};
 
-        updateColumns(activeColumn).items.push({
+        updateColumns[activeColumn].items.push({
             id:Date.now().toString(), 
             content : newTask,
         }); 
@@ -52,7 +52,7 @@ function Kanban() {
 
     const removeTask = (columnId,taskid) => { 
         const updatedColumns = {...columns}; 
-        updatedColumns[columnId].items = updatedColumns[columnId].items.filter((item) => item.id !== taskId);
+        updatedColumns[columnId].items = updatedColumns[columnId].items.filter((item) => item.id !== taskid);
         setColumns(updatedColumns)
     }
 
@@ -92,7 +92,7 @@ function Kanban() {
             border: "border-yellow-400"
         },
 
-        todo: { 
+        done: { 
             header: "bg-gradient-to-r from-green-600 to to-green-400", 
             border: "border-green-400"
         }
@@ -104,9 +104,80 @@ function Kanban() {
     <>
         <div className='p-6 w-full min-h-screen bg-gradient-to-b from-zinc-900 to-zinc-800 flex items-center justify-center'>
             <div className='flex items-center justify-center flex-col gap-4 w-full max-w-full-6xl'>
-                <h1 className='text-6xl font-bold mb-8 text-transparent'>
+                <h1 className='text-6xl font-bold mb-8 text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 via-amber-500 to-rose-400 '>
                     Kanban Board
                 </h1>
+
+                <div className='mb-8 flex w-full max-w-lg shadow-lg rounded-lg overflow-hidden'>
+                    <input type="text" value={newTask}
+                     onChange={(e) => setNewTask(e.target.value)}
+                     placeholder='Add a new Task'
+                     className='flex-grow p-3 bg-zinc-700 text-white'
+                    onKeyDown={(e)=> { e.key === "Enter" && addNewTask() }}
+                    />
+
+                    <select value={activeColumn}
+                    onChange={(e) => setActiveColumn(e.target.value)}
+                    className='p-3 bg-zinc-700 text-white border-0 border-1 border-zinc-600'
+                    >
+                        {Object.keys(columns).map((columnId) => (
+                            <option value={columnId} key={columnId}>
+                                {columns[columnId].name}
+                            </option>
+                        )
+                    )}
+
+
+                    </select>
+
+                    <button onClick={addNewTask}
+                    className='px-6 bg-gradient-to-r from-yellow-600 to-amber-500 text-white font-medium hover:from-yellow-500 hover:to-amber-400 transition-all duration-200 cursor-pointer'
+                    ></button>
+                </div>
+
+                <div className='flex gap-6 overflow-x-auto pb-6 w-full'>
+                    {Object.keys(columns).map((columnId)=> (
+                        <div key={columnId}
+                        className= {`flex-shrink-0 w-80 bg-zinc-800 rounded-lg shadow-xl border-t-4 ${columnStyles[columnId.border]}`}
+                        onDragOver={(e)=> handleDragOver(e,columnId)}
+                        onDrop={(e)=>handleDrop(e,columnId)}
+                        >
+                            <div className={`p-4 text-white font-bold text-xl rounded-t-md ${columnStyles[columnId].header}`}>
+                                {columns[columnId].name}
+                                <span className='ml-2 px-2 py-1 bg-zinc-800 bg-opacity-30 rounded-full text-sm '>
+                                    {columns[columnId].items.length}
+                                </span>
+                            </div>
+
+                            <div className='p-3 min-h-64'>
+                                {columns[columnId].items.length === 0 ? (
+                                    <div className='text-center py-10 text-zinc-500
+                                    italic text-sm'> 
+                                        Drop tasks here
+                                    </div>
+                                    ): (
+                                        columns[columnId].items.map((item)=> (
+                                            <div key={item.id} className='p-4 mb-3 bg-zinc-700 text-white rounded-lg shadow-md cursor-move flex items-center justify-between transform transition-all duration-200 hover:scale-105 hover:shadow-lg'
+                                            draggable
+                                            onDragStart={() => handleDragStart(columnId,item)}
+                                            >
+                                                <span className='mr-2'>{item.content}</span>
+                                                <button onClick={()=> removeTask(columnId,item.id)} 
+                                                className='bg-zinc-400 hover:text-red-400 transition-colors duration-200 w-6 h-6 flex justify-center items-center
+                                                rounded-full hover:bg-zinc-600'
+                                    
+                                                >
+                                                    <span className='text-lg cursor-pointer'></span>
+                                                </button>
+                                            </div>
+                                        ))
+                                    )
+                                }
+                            </div>
+
+                        </div>
+                    ))}
+                </div>
 
             </div>
 
